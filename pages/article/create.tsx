2315e852d389ca4586ce9c06aede6774e50835a3
darwin1224 <darwinblacks@gmail.com>
2019-11-25 21:25:13 +0700

    refactor(article:create): refactor some codes

--- pages/article/create.tsx
+++ pages/article/create.tsx
@@ -1,34 +1,47 @@
 import { Button, Form, Input } from 'antd';
+import { FormProps } from 'antd/lib/form';
 import Axios from 'axios';
 import Link from 'next/link';
-import React, { ChangeEvent, Component, FormEvent, ReactNode } from 'react';
+import Router from 'next/router';
+import React, { ChangeEvent, FormEvent, PureComponent, ReactNode } from 'react';
 import BaseLayout from '../../components/BaseLayout';
+import { ArticleCreateState, ArticleModel } from '../../typings/states/article';
 
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 1 },
-    sm: { span: 1 },
-  },
-  wrapperCol: {
-    xs: { span: 1 },
-    sm: { span: 4 },
-  },
-};
-
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 4,
-      offset: 0,
+export default class ArticleCreatePage extends PureComponent<{}, ArticleCreateState> {
+  /**
+   * Form item layout
+   *
+   * @type {FormProps}
+   */
+  private readonly formItemLayout: FormProps = {
+    labelCol: {
+      xs: { span: 1 },
+      sm: { span: 1 },
     },
-    sm: {
-      span: 16,
-      offset: 1,
+    wrapperCol: {
+      xs: { span: 8 },
+      sm: { span: 8 },
     },
-  },
-};
+  };
 
-export default class ArticleCreatePage extends Component<{}, ArticleCreateState> {
+  /**
+   * Tail form item layout
+   *
+   * @type {FormProps}
+   */
+  private readonly tailFormItemLayout: FormProps = {
+    wrapperCol: {
+      xs: {
+        span: 4,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 1,
+      },
+    },
+  };
+
   /**
    * Constructor
    *
@@ -68,8 +81,11 @@ export default class ArticleCreatePage extends Component<{}, ArticleCreateState>
   private async handleSubmit(e: FormEvent<HTMLFormElement>): Promise<void> {
     e.preventDefault();
     this.setState({ isLoading: true });
-    await Axios.post('https://jsonplaceholder.typicode.com/posts');
+    await Axios.post<ArticleModel>('https://jsonplaceholder.typicode.com/posts', {
+      ...this.state,
+    });
     this.setState({ isLoading: false });
+    await Router.push('/article');
   }
 
   /**
@@ -81,14 +97,24 @@ export default class ArticleCreatePage extends Component<{}, ArticleCreateState>
     return (
       <>
         <BaseLayout>
-          <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <Form {...this.formItemLayout} onSubmit={this.handleSubmit}>
             <Form.Item label="Title">
-              <Input name="title" value={this.state.title} onChange={this.handleInputChange} />
+              <Input
+                name="title"
+                placeholder="Enter Title"
+                value={this.state.title}
+                onChange={this.handleInputChange}
+              />
             </Form.Item>
             <Form.Item label="Body">
-              <Input name="body" value={this.state.body} onChange={this.handleInputChange} />
+              <Input
+                name="body"
+                placeholder="Enter Body"
+                value={this.state.body}
+                onChange={this.handleInputChange}
+              />
             </Form.Item>
-            <Form.Item {...tailFormItemLayout}>
+            <Form.Item {...this.tailFormItemLayout}>
               <Button
                 type="primary"
                 htmlType="submit"
