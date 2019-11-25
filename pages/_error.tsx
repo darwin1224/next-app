e17642ec84b31fac99a61198f6f378751551fcea
Darwin <darwinblacks@gmail.com>
2019-11-25 15:39:44 +0700

    feat: add error page

---
+++ pages/_error.tsx
@@ -0,0 +1,20 @@
+import React, { Component, ReactNode } from 'react';
+
+export default class ErrorPage extends Component {
+  public render(): ReactNode {
+    return (
+      <>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <h1>404 | Page Not Found</h1>
+        </div>
+      </>
+    );
+  }
+}
